8ed152a04a1711c833a0e027f007ec294a8215df
MikasaAkerman <benjamin@benjamin-pcdeMacBook-Pro.local>
2019-06-19 15:51:15 +0800

    add preprocessor

--- echo.c
+++ echo.c
@@ -1,21 +1,51 @@
-#include "stdio.h"
+
+/* If we are compiling on Windows compile these functions */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {}
+
+/* Otherwise include the editline headers */
+#else
+#ifdef __APPLE__
+#include <editline/readline.h>
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
+#endif
 
 #define bufSize 2048
 
 static char* version = "Lispy version 0.0.0.1";
-static char* exit = "Press Ctrl+C to quit";
-static char input[bufSize];
+static char* exits = "Press Ctrl+C to quit";
 
-int main(int argc, char const *argv[])
+int echo()
 {
     puts(version);
-    puts(exit);
+    puts(exits);
 
     while (1)
     {
-        fputs("lispy>", stdout);
-        fgets(input, bufSize, stdin);
-        printf("%s", input);
+        char* input = readline("lispy>");
+        add_history(input);
+
+        printf("%s\n", input);
+
+        free(input);
     }
 
     return 0;
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "echo.c"
+
+int main(int argc, char** argv) {
+
+  echo();
+  return 0;
+}
